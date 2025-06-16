bd4364113535449d9c0686a35943eb0b2fc70596
RioAPP010 <rioandhikaa702@gmail.com>
2025-06-16 12:20:24 +0700

    cre

--- script.js
+++ script.js
@@ -5,14 +5,14 @@ const supabase = createClient(
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBjYnp4YXBramFoZG5tbHB6c2N4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk5MTU0MDcsImV4cCI6MjA2NTQ5MTQwN30.eZmuw9ioVIqhaTBbEZkmbmDJAVwsMRqPf7bRBoaqY94"
 )
 
-async function  simpanData() {
+async function simpanData() {
     const nama = document.getElementById("nama").value;
     const username_ig = document.getElementById("username_ig").value;
 
-    const { data, error } = await supabase.from("buku_tamu").insert([{nama , username_ig}]);
+    const { data, error } = await supabase.from("buku_tamu").insert([{ nama, username_ig }]);
 
     if (error) return alert("Gagal memasukkan data " + error.message)
-        alert("Data berhasil ditambahkan")
+    alert("Data berhasil ditambahkan")
 }
 
 window.simpanData = simpanData;
